88801d0fd74164d50eedec409ae13de4b02f44f6
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-02-19 01:42:34 +0700

    Update port in Heroku

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.get('/about', (req, res) => {
   res.send('Kiet!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
+
+const PORT = process.env.PORT || 7703;
+app.listen(PORT, () => {
+  console.log(`Our app is running on port ${PORT}`);
+});
