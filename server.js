90383dfe469502488866d75444cfafa6466f351c
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-02-19 01:31:57 +0700

    Declare Route

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+const port = 7703
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/about', (req, res) => {
+  res.send('Kiet!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
