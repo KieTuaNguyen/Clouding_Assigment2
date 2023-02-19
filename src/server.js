653625dd420d19147b78ea1caf90270b4e884005
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-02-19 21:17:23 +0700

    Connected MongoDB Atlas

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+/*Database*/
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false);
+
+const database = (module.exports = () => {
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+  try {
+    mongoose.connect(
+      "mongodb+srv://ClusterATN:kietntgdd210002@clusteratn.cz23uub.mongodb.net/assignment2?retryWrites=true&w=majority",
+      connectionParams
+    );
+    console.log("Database connected succesfully");
+  } catch (error) {
+    console.log(error);
+    console.log("Database connection failed");
+  }
+});
+database();
+
+app.get('/', (req, res) => {
+  // res.send('Hello Kiet!')
+  res.sendFile(path.join(__dirname, 'index.ejs'));
+})
+
+const PORT = process.env.PORT || 7703;
+app.listen(PORT, () => {
+  console.log(`Our app is running at http://localhost:${PORT}`);
+});
